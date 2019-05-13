384eca7d5ec0e458fb0d26230cef6ab9a6fc0660
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-13 16:40:19 +0100

    working log in and signup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
 import React from "react";
 import { Router } from "@reach/router";
 import User from "./views/user";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./views/signup";
 import Login from "./views/login";
-import "shards-ui/dist/css/shards.min.css";
 import NavMenu from "./components/nav";
-import { Container, Row } from "shards-react";
+import { Container } from "shards-react";
+import { navigate } from "@reach/router";
 import Home from "./views/home";
-
+import { getUserInfo } from "./api/api";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 import "./style.css";
 
 class App extends React.Component {
-  state = { username: "", token: "" };
+  state = { loggedInUser: "", token: "", avatar_url: "" };
   render() {
+    const { loggedInUser, avatar_url } = this.state;
     return (
-      <Container className="col-12">
-        <Row>
-          <NavMenu />
-        </Row>
-        <Router>
-          <Home path="/" />
-          <User path="/user/:author" />
-          <Signup path="/signup" />
-          <Login login={this.login} path="/login" />
-        </Router>
-        ;
-      </Container>
+      <div>
+        <NavMenu logout={this.logout} loggedInUser={this.state.loggedInUser} />
+
+        <Container className="col-12">
+          <Router>
+            <Home
+              avatar_url={avatar_url}
+              loggedInUser={loggedInUser}
+              path="/"
+            />
+            <User path="/user/:author" />
+            <Signup path="/signup" />
+            <Login login={this.login} path="/login" />
+          </Router>
+          ;
+        </Container>
+      </div>
     );
   }
   login = (username, token) => {
-    console.log(username, token);
     this.setState({ loggedInUser: username, token });
+  };
+  logout = () => {
+    this.setState({ loggedInUser: "", token: "" });
+    navigate("/");
+  };
+  componentDidUpdate = () => {
+    const { loggedInUser } = this.state;
+    if (loggedInUser) {
+      return getUserInfo(loggedInUser).then(data => {
+        this.setState({ avatar_url: data.user.avatar_url });
+      });
+    }
   };
 }
 
