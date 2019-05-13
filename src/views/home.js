32eba139c2894ef2d5609cee9ff01e1b7a76b558
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-13 16:42:07 +0100

    implimented user dash to home

--- src/views/home.js
+++ src/views/home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Row, Col } from "shards-react";
 import Articles from "../components/articles";
-
-const Home = () => {
+import UserDash from "../components/user-dash";
+const Home = ({ loggedInUser, avatar_url }) => {
   return (
     <Row>
-      <Col cols="12" md="4" lg="3">
-        userinfo
+      <Col cols="3" md="3" lg="3">
+        <UserDash avatar_url={avatar_url} loggedInUser={loggedInUser} />
       </Col>
-      <Col cols="12" md="4" lg="6">
+      <Col cols="6" md="6" lg="6">
         <Articles />;
       </Col>
     </Row>
