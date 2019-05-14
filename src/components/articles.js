cd9fae50f30f34e7e6eb3551f28c570a33ee6517
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-14 16:39:10 +0100

    wrapped articles in infinate scroll

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,43 +1,69 @@
 import React from "react";
 import Article from "./article";
-const axios = require("axios");
+import InfiniteScroll from "react-infinite-scroller";
+import { getArticles } from "../api/api";
+
 class Articles extends React.Component {
   state = {
-    articles: []
+    articles: [],
+    moreArticles: true
   };
   render() {
-    const { articles } = this.state;
-    return articles.map(article => {
-      const {
-        votes,
-        article_id,
-        comment_count,
-        author,
-        title,
-        body,
-        created_at
-      } = article;
-      return (
-        <Article
-          key={article_id}
-          comment_count={comment_count}
-          article_id={article_id}
-          author={author}
-          title={title}
-          body={body}
-          created_at={created_at}
-          votes={votes}
-        />
-      );
-    });
+    const { articles, moreArticles } = this.state;
+    return (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={this.changePage}
+        hasMore={true || false}
+        loader={
+          moreArticles && (
+            <div className="loader" key={0}>
+              Loading ...
+            </div>
+          )
+        }
+      >
+        {articles.map(article => {
+          const {
+            votes,
+            article_id,
+            comment_count,
+            author,
+            title,
+            body,
+            created_at
+          } = article;
+          return (
+            <Article
+              key={article_id}
+              comment_count={comment_count}
+              article_id={article_id}
+              author={author}
+              title={title}
+              body={body}
+              created_at={created_at}
+              votes={votes}
+            />
+          );
+        })}
+      </InfiniteScroll>
+    );
   }
   componentDidMount = () => {
-    const authorQuery = this.props.author ? `?author=${this.props.author}` : "";
-    return axios
-      .get(`https://pure-falls-39051.herokuapp.com/api/articles${authorQuery}`)
-      .then(({ data }) => {
-        this.setState({ articles: data.articles });
-      });
+    let config = { author: this.props.author, page: this.state.page };
+    getArticles(config).then(articles => {
+      this.setState({ articles });
+    });
+  };
+  changePage = p => {
+    let config = { author: this.props.author, page: p };
+    getArticles(config).then(articles => {
+      if (articles) {
+        this.setState({ articles: [...this.state.articles, ...articles] });
+      } else {
+        this.setState({ moreArticles: false });
+      }
+    });
   };
 }
 
