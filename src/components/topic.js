be663b56e8e820752dbc915d4663eda1f644c0d8
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-15 15:44:33 +0100

    removed rainbow colors

--- src/components/topic.js
+++ src/components/topic.js
@@ -3,14 +3,6 @@ import React from "react";
 import { Badge } from "shards-react";
 
 const Topic = ({ topic }) => {
-  return (
-    <Badge
-      style={{
-        backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`
-      }}
-    >
-      {topic}
-    </Badge>
-  );
+  return <Badge theme="primary">{topic}</Badge>;
 };
 export default Topic;
