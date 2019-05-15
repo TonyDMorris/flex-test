27aaafeaf8084a079cfbddfd11bb251ca880db9a
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-15 08:37:00 +0100

    fixed infinate scroll errors from server

--- src/components/articles.js
+++ src/components/articles.js
@@ -10,45 +10,47 @@ class Articles extends React.Component {
   };
   render() {
     const { articles, moreArticles } = this.state;
+
     return (
-      <InfiniteScroll
-        pageStart={0}
-        loadMore={this.changePage}
-        hasMore={true || false}
-        loader={
-          moreArticles && (
-            <div className="loader" key={0}>
+      articles.length && (
+        <InfiniteScroll
+          pageStart={1}
+          loadMore={this.changePage}
+          hasMore={moreArticles}
+          loader={
+            <div className="loader" key={1000}>
               Loading ...
             </div>
-          )
-        }
-      >
-        {articles.map(article => {
-          const {
-            votes,
-            article_id,
-            comment_count,
-            author,
-            title,
-            body,
-            created_at
-          } = article;
-          return (
-            <Article
-              key={article_id}
-              comment_count={comment_count}
-              article_id={article_id}
-              author={author}
-              title={title}
-              body={body}
-              created_at={created_at}
-              votes={votes}
-            />
-          );
-        })}
-      </InfiniteScroll>
+          }
+        >
+          {articles.map(article => {
+            const {
+              votes,
+              article_id,
+              comment_count,
+              author,
+              title,
+              body,
+              created_at
+            } = article;
+            return (
+              <Article
+                key={article_id}
+                comment_count={comment_count}
+                article_id={article_id}
+                author={author}
+                title={title}
+                body={body}
+                created_at={created_at}
+                votes={votes}
+              />
+            );
+          })}
+        </InfiniteScroll>
+      )
     );
   }
+
   componentDidMount = () => {
     let config = { author: this.props.author, page: this.state.page };
     getArticles(config).then(articles => {
@@ -57,13 +59,13 @@ class Articles extends React.Component {
   };
   changePage = p => {
     let config = { author: this.props.author, page: p };
-    getArticles(config).then(articles => {
-      if (articles) {
+    getArticles(config)
+      .then(articles => {
         this.setState({ articles: [...this.state.articles, ...articles] });
-      } else {
+      })
+      .catch(({ error }) => {
         this.setState({ moreArticles: false });
-      }
-    });
+      });
   };
 }
 
