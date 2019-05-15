3bcd2df8e57ba4c4ad54b89e71f9e953454f6bd9
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-15 12:34:34 +0100

    made style changes and added topic

--- src/components/article.js
+++ src/components/article.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Topic from "../components/topic";
 import ArticleUser from "./article-user";
 import { Link } from "@reach/router";
 
@@ -8,22 +8,39 @@ import {
   CardHeader,
   CardTitle,
   CardBody,
-  CardFooter,
-  CardSubtitle
+  CardSubtitle,
+  Col,
+  Row
 } from "shards-react";
 
 const Article = ({
   author,
   title,
   created_at,
-
+  topic,
   article_id,
-  votes
+  votes,
+  token
 }) => {
   return (
     <Card style={{ marginTop: "10px" }} small={true}>
       <CardHeader>
-        <ArticleUser author={author} />
+        <Row>
+          <Col className="offset-11">
+            {votes}
+            <span role="img" aria-label="muscle emoji">
+              {votes >= 0 ? "💪" : "😠"}
+            </span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ArticleUser author={author} />
+          </Col>
+          <Col className="offset-10">
+            <Topic topic={topic} />
+          </Col>
+        </Row>
       </CardHeader>
 
       <CardBody>
@@ -34,15 +51,6 @@ const Article = ({
           {created_at}
         </CardSubtitle>
       </CardBody>
-
-      <CardFooter>
-        <div className="offset-md-11">
-          {votes}
-          <span role="img" aria-label="muscle emoji">
-            💪
-          </span>
-        </div>
-      </CardFooter>
     </Card>
   );
 };
