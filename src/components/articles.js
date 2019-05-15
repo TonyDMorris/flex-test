f62c3e08d7e5ac573805dbe87a8b0a3a298a1df1
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-15 15:43:29 +0100

    added additional params to state for sorting

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,64 +1,67 @@
 import React from "react";
 import Article from "./article";
 import InfiniteScroll from "react-infinite-scroller";
+import DropDownSort from "./drop-down-sort";
 import { getArticles } from "../api/api";
 
 class Articles extends React.Component {
   state = {
     articles: [],
-    moreArticles: true
+    moreArticles: true,
+    sort_by: "created_at",
+    order: "desc"
   };
   render() {
     const { articles, moreArticles } = this.state;
 
     return (
       articles.length && (
-        <InfiniteScroll
-          pageStart={1}
-          loadMore={this.changePage}
-          hasMore={moreArticles}
-          loader={
-            <div className="loader" key={1000}>
-              Loading ...
-            </div>
-          }
-        >
-          {articles.map(article => {
-            const {
-              topic,
-              votes,
-              article_id,
-              comment_count,
-              author,
-              title,
-              body,
-              created_at
-            } = article;
-            return (
-              <Article
-                token={this.props.token}
-                topic={topic}
-                key={article_id}
-                comment_count={comment_count}
-                article_id={article_id}
-                author={author}
-                title={title}
-                body={body}
-                created_at={created_at}
-                votes={votes}
-              />
-            );
-          })}
-        </InfiniteScroll>
+        <React.Fragment>
+          <DropDownSort handleSort={this.handleSort} />
+          <InfiniteScroll
+            pageStart={1}
+            loadMore={this.changePage}
+            hasMore={moreArticles}
+            loader={
+              <div className="loader" key={1000}>
+                Loading ...
+              </div>
+            }
+          >
+            {articles.map(article => {
+              const {
+                topic,
+                votes,
+                article_id,
+                comment_count,
+                author,
+                title,
+                body,
+                created_at
+              } = article;
+              return (
+                <Article
+                  token={this.props.token}
+                  topic={topic}
+                  key={article_id}
+                  comment_count={comment_count}
+                  article_id={article_id}
+                  author={author}
+                  title={title}
+                  body={body}
+                  created_at={created_at}
+                  votes={votes}
+                />
+              );
+            })}
+          </InfiniteScroll>
+        </React.Fragment>
       )
     );
   }
 
   componentDidMount = () => {
-    let config = { author: this.props.author, page: this.state.page };
-    getArticles(config).then(articles => {
-      this.setState({ articles });
-    });
+    this.fetchArticles();
   };
   changePage = p => {
     let config = { author: this.props.author, page: p };
@@ -69,6 +72,23 @@ class Articles extends React.Component {
       .catch(({ error }) => {
         this.setState({ moreArticles: false });
       });
+  };
+  handleSort = sort_by => {
+    this.setState({ sort_by }, () => {
+      this.fetchArticles();
+    });
+  };
+  fetchArticles = () => {
+    const { page, sort_by, order } = this.state;
+    let config = {
+      author: this.props.author,
+      page,
+      sort_by,
+      order
+    };
+    getArticles(config).then(articles => {
+      this.setState({ articles });
+    });
   };
 }
 
