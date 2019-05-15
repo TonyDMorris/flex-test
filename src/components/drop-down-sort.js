69b071b4d370ff98c80f5dd6fbad038ca3bb2a16
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-15 15:44:14 +0100

    stolen dropdown from shards site and modified

--- src/components/drop-down-sort.js
+++ src/components/drop-down-sort.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dropdown,
   DropdownToggle,
@@ -7,49 +7,51 @@ import {
   Button
 } from "shards-react";
 
-class DropDownSort extends React.Component {
-  state = { open: false, sort_by: "created_at" };
+const DropDownSort = props => {
+  const [sort_by, setSort] = useState("created_at");
+  const [open, setOpen] = useState(false);
 
-  render() {
-    return (
-      <Dropdown open={this.state.open} toggle={this.toggle} group>
-        <Button
-          onClick={() => {
-            this.props.handleSort(this.state.sort_by);
+  return (
+    <Dropdown
+      open={open}
+      toggle={e => {
+        setOpen(!open);
+      }}
+      group
+    >
+      <Button
+        onClick={() => {
+          props.handleSort(sort_by);
+        }}
+      >
+        Sort
+      </Button>
+      <DropdownToggle split />
+      <DropdownMenu>
+        <DropdownItem
+          onClick={e => {
+            setSort("created_at");
           }}
         >
-          Sort
-        </Button>
-        <DropdownToggle split />
-        <DropdownMenu>
-          <DropdownItem
-            onClick={e => {
-              this.setState({ sort_by: "created_at" });
-            }}
-          >
-            Creation date
-          </DropdownItem>
-          <DropdownItem
-            onClick={e => {
-              this.setState({ sort_by: "author" });
-            }}
-          >
-            Author
-          </DropdownItem>
-          <DropdownItem
-            onClick={e => {
-              this.setState({ sort_by: "votes" });
-            }}
-          >
-            Flex
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    );
-  }
-  toggle() {
-    this.setState({ open: !this.state.open });
-  }
-}
+          Creation date
+        </DropdownItem>
+        <DropdownItem
+          onClick={e => {
+            setSort("author");
+          }}
+        >
+          Author
+        </DropdownItem>
+        <DropdownItem
+          onClick={e => {
+            setSort("votes");
+          }}
+        >
+          Flex
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
 
 export default DropDownSort;
