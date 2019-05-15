a6ca21f4ce59f955f52c7f794eeb72e7ea06c11c
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-15 12:36:33 +0100

    added token to single article

--- src/views/single-article.js
+++ src/views/single-article.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ArticleMain from "../components/article-main";
-const SingleArticle = ({ article_id, loggedInUser }) => {
+const SingleArticle = ({ article_id, loggedInUser, token }) => {
   return (
     <div className="col-8 offset-2">
-      <ArticleMain loggedInUser={loggedInUser} article_id={article_id} />
+      <ArticleMain
+        token={token}
+        loggedInUser={loggedInUser}
+        article_id={article_id}
+      />
     </div>
   );
 };
