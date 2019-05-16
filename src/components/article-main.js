532b22f09392d46fa436a761855a16af5e056276
TonyDMorris <anthonymorris86@googlemail.com>
2019-05-16 16:35:05 +0100

    removed useless import

--- src/components/article-main.js
+++ src/components/article-main.js
@@ -6,7 +6,7 @@ import VoteBar from "../components/vote-bar";
 import Topic from "./topic";
 import { patchVotes, getArticle, deleteArticle } from "../api/api";
 import { Link, navigate } from "@reach/router";
-import {} from "../api/api";
+
 import {
   Card,
   CardHeader,
@@ -43,94 +43,111 @@ class ArticleMain extends React.Component {
       created_at,
       topic
     } = this.state;
-    return title && loggedInUser ? (
-      <Card style={{ marginTop: "10px" }} small={false}>
-        <CardHeader>
-          <Container>
-            <Row>
-              <Col>
-                <ArticleUser author={author} />
-              </Col>
-              <Col className=" col-1 align-self-end">
-                {loggedInUser === author && (
-                  <Button
-                    onClick={() => {
-                      this.removeArticle();
-                    }}
-                  >
-                    X
-                  </Button>
-                )}
-              </Col>
-            </Row>
-          </Container>
-        </CardHeader>
+    return (
+      title && (
+        <Card style={{ marginTop: "10px" }} small={false}>
+          <CardHeader>
+            <Container>
+              <Row>
+                <Col>
+                  <ArticleUser author={author} />
+                </Col>
+                <Col className=" col-1 align-self-end">
+                  {loggedInUser === author && (
+                    <Button
+                      onClick={() => {
+                        this.removeArticle();
+                      }}
+                    >
+                      X
+                    </Button>
+                  )}
+                </Col>
+              </Row>
+            </Container>
+          </CardHeader>
 
-        <CardBody>
-          <CardTitle>{title}</CardTitle>
+          <CardBody>
+            <CardTitle>{title}</CardTitle>
 
-          <CardSubtitle style={{ colour: "grey", fontSize: "10px" }}>
-            {created_at}
-          </CardSubtitle>
+            <CardSubtitle style={{ colour: "grey", fontSize: "10px" }}>
+              {created_at}
+            </CardSubtitle>
 
-          <p>{`${body}`}</p>
+            <p>{`${body}`}</p>
 
-          {loggedInUser ? (
-            <Comments
-              loggedInUser={loggedInUser}
-              token={token}
-              comment_count={comment_count}
-              article_id={article_id}
-            />
-          ) : (
-            <div>
-              <Link to="/signup">Signup</Link> or <Link to="/login">login</Link>{" "}
-              to see the comments
-            </div>
-          )}
-        </CardBody>
+            {loggedInUser ? (
+              <Comments
+                increaseCommentCount={this.increaseCommentCount}
+                loggedInUser={loggedInUser}
+                token={token}
+                comment_count={comment_count}
+                article_id={article_id}
+              />
+            ) : (
+              <div>
+                <Link to="/signup">Signup</Link> or{" "}
+                <Link to="/login">login</Link> to see the comments
+              </div>
+            )}
+          </CardBody>
 
-        <CardFooter>
-          <Container>
-            <Row>
-              <Col>
-                <Topic topic={topic} />
-              </Col>
-              <Col className="col-auto">
-                <VoteBar
-                  media_id={article_id}
-                  incrementVotes={this.incrementVotes}
-                  votes={votes}
-                />
-              </Col>
-            </Row>
-          </Container>
-        </CardFooter>
-      </Card>
-    ) : (
-      <CardBody className="col-6 offset-4">
-        <Link to="/signup">Signup</Link> or <Link to="/login">Login</Link> to
-        see the full article
-      </CardBody>
+          <CardFooter>
+            <Container>
+              <Row>
+                <Col>
+                  <Topic topic={topic} />
+                </Col>
+                <Col className="col-auto">
+                  <VoteBar
+                    media_id={article_id}
+                    incrementVotes={this.incrementVotes}
+                    votes={votes}
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </CardFooter>
+        </Card>
+      )
     );
   }
   incrementVotes = (id, isComment, n) => {
-    patchVotes(id, false, n, this.props.token).then(article => {
-      const newVotes = this.state.votes + n;
-      this.setState({ votes: newVotes });
-    });
+    patchVotes(id, false, n, this.props.token)
+      .then(article => {
+        const newVotes = this.state.votes + n;
+        this.setState({ votes: newVotes });
+      })
+      .catch(() => {
+        navigate("/error", {
+          replace: true,
+          state: { msg: "dunno why this would error" }
+        });
+      });
   };
   componentDidMount = () => {
     const { article_id } = this.props;
-    getArticle(article_id).then(article => {
-      this.setState({ ...article });
-    });
+    getArticle(article_id)
+      .then(article => {
+        this.setState({ ...article });
+      })
+      .catch(() => {
+        navigate("/error", {
+          replace: true,
+          state: { msg: "you know full well this doesn't exist" }
+        });
+      });
   };
   removeArticle = () => {
-    console.log("im here");
     const { article_id } = this.props;
     deleteArticle(article_id, this.props.token);
     navigate("/article-deleted", { replace: true });
+  };
+
+  increaseCommentCount = () => {
+    let { comment_count } = this.state;
+    const newCount = +comment_count + 1;
+    this.setState({ comment_count: newCount });
   };
 }
 
